0bcbfcfbe2808e52afd0cd32607c4c783d5c3a00
jhammarstedt <johan.hammarstedt@gmail.com>
2020-12-07 00:23:22 +0100

    fixes on d6

--- d6/d6.py
+++ d6/d6.py
@@ -2,7 +2,6 @@ import sys
 
 def count(g:list)->int:
     return len(set(''.join(g))) #count the unique values in the group
-
 def task1(data:list):
     group = []
     yes = 0
@@ -16,7 +15,6 @@ def task1(data:list):
                 yes+= count(group)
         
     print(yes)
-
 def task2(data:list):
     yes =0
     group =[]
@@ -28,20 +26,14 @@ def task2(data:list):
             group.append(set(i))
             if ind == len(data)-1: #last one
                 yes += len(set.intersection(*group))     
-       
     print(yes)
-
-
 def main():
     data = []
     for input in sys.stdin:
         data.append(input.strip())
     
-    #task1()
+    task1(data)
     task2(data)
     
-   
-
-
 if __name__ == "__main__":
     main()
